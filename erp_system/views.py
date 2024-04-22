ff2e95d8c82c379dff5d699131ac59f55fcfbce0
--- <ebers.ideas@gmail.com>
2024-04-22 14:09:27 -0600

    login_required

--- erp_system/views.py
+++ erp_system/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-@login_required
+#@login_required
 def incio(request):
     return render(request, 'incio.html', {'api_url': settings.API_URL})
