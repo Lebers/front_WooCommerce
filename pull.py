3e5c49a47d3ee61d16004ec331729851db9db4d4
--- <code.lebers@gmail.com>
2024-04-24 13:39:21 -0600

    Create pull.py

---
+++ pull.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+from dotenv import load_dotenv
+
+# Carga las variables de entorno del archivo .env
+load_dotenv()
+
+# Obtiene el nombre del servicio desde el archivo .env
+service_name = os.getenv('SERVICE_NAME')
+
+def run_command(command):
+    """ Ejecuta un comando en el shell y retorna su salida. """
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.stdout + result.stderr
+    except subprocess.CalledProcessError as e:
+        return e.output + e.stderr
+
+def main():
+    # Ejecuta git pull
+    print("Ejecutando git pull...")
+    print(run_command("git pull"))
+
+    # Detiene el servicio
+    print(f"Deteniendo el servicio {service_name}...")
+    print(run_command(f"sudo systemctl stop {service_name}"))
+
+    # Inicia el servicio
+    print(f"Iniciando el servicio {service_name}...")
+    print(run_command(f"sudo systemctl start {service_name}"))
+
+    # Obtiene el estado del servicio
+    print(f"Estado del servicio {service_name}:")
+    print(run_command(f"sudo systemctl status {service_name}"))
+
+if __name__ == "__main__":
+    main()
