0364c15bbc51756bc9ab7794d076aaba4154dc99
--- <code.lebers@gmail.com>
2024-04-25 02:11:31 -0600

    lista de usuaiors

--- erp_system/views.py
+++ erp_system/views.py
@@ -4,20 +4,23 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 
-#@login_required
+@login_required
 def incio(request):
     return render(request, 'incio.html', {'api_url': settings.API_URL})
 
 #def product(request):
 #    return render(request, 'product.html', {'api_url': settings.API_URL})
+@login_required
 def product(request, id=None):
     # Solo se pasa el ID al contexto si existe
     context = {'product_id': id} if id else {}
     return render(request, 'product.html', context)
 
+@login_required
 def listProduct(request):
     return render(request, 'list_product.html', {'api_url': settings.API_URL})
 
+@login_required
 def specialCatalogs(request, catalogoName=None):
     # Pasamos tanto la base URL de la API como el nombre del catálogo a la plantilla
     context = {
@@ -26,14 +29,22 @@ def specialCatalogs(request, catalogoName=None):
     }
     return render(request, 'special_catalogs.html', context)
 
-
+@login_required
 def newUser(request):
     return render(request, 'new_user_wherehouse.html', {'api_url': settings.API_URL})
+
+@login_required
 def listUsers(request):
     return render(request, 'list_users.html', {'api_url': settings.API_URL})
+
+@login_required
 def newWereHouse(request):
     return render(request, 'new_werehouse.html', {'api_url': settings.API_URL})
+
+@login_required
 def listWere(request):
     return render(request, 'list_wherehouse.html', {'api_url': settings.API_URL})
+
+@login_required
 def listProducts(request):
     return render(request, 'list_products.html', {'api_url': settings.API_URL})
--- usuario/views.py
+++ usuario/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def nuevo_usuario(request):
     return render(request, 'nuevo.html')
 
+@login_required
 def buscar(request):
     return render(request, 'buscar.html')
 
+@login_required
 def nuevo_usuario_warehouse(request):
     return render(request, 'nuevo_usuario_warehouse.html')
